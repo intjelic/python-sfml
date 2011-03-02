d0f5133db8b70999115e6d7d331a099978a3e1b5
klavdij <devnull@localhost>
2011-03-02 21:11:15 +0100

    smal fixes

--- examples/renderimage.py
+++ examples/renderimage.py
@@ -2,14 +2,33 @@
 # -*- coding: utf-8 -*-
 
 import sf
+import sys
 
 
 def main():
-    window = sf.RenderWindow(sf.VideoMode(640, 480), 'Sprite example')
+    if not sf.RenderImage.IS_AVAILABLE:
+        print "Your system doesn't support RenderImage"
+        sys.exit(1)
+    
+    window = sf.RenderWindow(sf.VideoMode(640, 480), 'Shape example')
     window.framerate_limit = 60
     running = True
     image = sf.Image.load_from_file('python-logo.png')
-    sprite = sf.Sprite(image)
+    circle0 = sf.circle(50, 50, 50, sf.Color.YELLOW)
+    circle0.position = 100, 100
+    
+    rect0 = sf.rectangle(5, 5, 90, 50, sf.Color.GREEN, 2, sf.Color.BLUE)
+    
+    rect1 = sf.rectangle_from_rect(sf.FloatRect(20.0, 30.0, 50.0, 50.0),
+                                   sf.Color.CYAN)
+    ri = sf.RenderImage(110, 110)
+    ri.clear(sf.Color(0, 0, 0, 0))
+    ri.draw(rect0)
+    ri.draw(rect1)
+    ri.display()
+    s = sf.Sprite(ri.image)
+    s.origin = 55, 55
+    s.position = 320, 240
 
     while running:
         for event in window.iter_events():
@@ -17,7 +36,8 @@ def main():
                 running = False
 
         window.clear(sf.Color.WHITE)
-        window.draw(sprite)
+        s.rotate(5)
+        window.draw(s)
         window.display()
     window.close()
 
--- examples/shape.py
+++ examples/shape.py
@@ -2,14 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import sf
-import sys
 
 
 def main():
-    if not sf.RenderImage.IS_AVAILABLE:
-        print "Your system doesn't support RenderImage"
-        sys.exit(1)
-    
     window = sf.RenderWindow(sf.VideoMode(640, 480), 'Shape example')
     window.framerate_limit = 60
     running = True
@@ -17,18 +12,22 @@ def main():
     circle0 = sf.circle(50, 50, 50, sf.Color.YELLOW)
     circle0.position = 100, 100
     
-    rect0 = sf.rectangle(5, 5, 90, 50, sf.Color.GREEN, 2, sf.Color.BLUE)
+    rect0 = sf.rectangle(0, 0, 100, 50, sf.Color.GREEN, 2, sf.Color.BLUE)
+    rect0.position = 200, 400
     
-    rect1 = sf.rectangle_from_rect(sf.FloatRect(20.0, 30.0, 50.0, 50.0),
+    rect1 = sf.rectangle_from_rect(sf.FloatRect(0.0, 0.0, 50.0, 50.0),
                                    sf.Color.CYAN)
-    ri = sf.RenderImage(110, 110)
-    ri.clear(sf.Color(0, 0, 0, 0))
-    ri.draw(rect0)
-    ri.draw(rect1)
-    ri.display()
-    s = sf.Sprite(ri.image)
-    s.origin = 55, 55
-    s.position = 320, 240
+    rect1.position = 400, 100
+    
+    line0 = sf.line(0, 0, 640, 480, 5, sf.Color.RED)
+    
+    p_list = [(0.0, 50.0), (50.0, 50.0), (25.0, 0.0)]
+    points = [sf.ShapePoint(x, y) for (x, y) in p_list]
+    shape0 = sf.Shape(points)
+    shape0.position = 300, 200
+    shape0.color = sf.Color(50, 100, 200, 128)
+    
+    shapes = [line0, circle0, rect0, rect1, shape0]
 
     while running:
         for event in window.iter_events():
@@ -36,8 +35,9 @@ def main():
                 running = False
 
         window.clear(sf.Color.WHITE)
-        s.rotate(5)
-        window.draw(s)
+        shape0.rotate(2)
+        for s in shapes:
+            window.draw(s)
         window.display()
     window.close()
 
