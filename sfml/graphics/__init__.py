4641b74c9c07e635fc34c4c07097cd4b4555817e
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2012-01-13 08:12:35 +0100

    Modules import now their dependencies when we import them separatly

--- sfml/graphics/__init__.py
+++ sfml/graphics/__init__.py
@@ -2,11 +2,17 @@ __all__ = ['BlendMode', 'Matrix3', 'Color', 'Glyph', 'Font', 'Image',\
  'Texture', 'Drawable', 'Text', 'Sprite', 'Shape', 'View', 'Shader',\
   'RenderTarget', 'RenderWindow', 'RenderTexture', 'Renderer', 'Style',
   'HandledWindow']
+  
 
-#from sfml.system import *
-#from sfml.window import *
 from sfml.graphics.graphics import BlendMode, Matrix3, Color, Glyph, \
 Font, Image, Texture, Drawable, Text, Sprite, Shape, View, Shader, \
 RenderTarget, RenderWindow, RenderTexture, Renderer, Style, \
 HandledWindow
 
+from sfml.graphics.graphics import ContextSettings, Event, Joystick, Keyboard, Mouse, VideoMode, Window
+
+from sfml.graphics.graphics import SFMLException
+from sfml.graphics.graphics import sleep, Clock
+from sfml.system.position import Position
+from sfml.system.size import Size
+from sfml.system.rectangle import Rectangle
--- sfml/window/__init__.py
+++ sfml/window/__init__.py
@@ -1,5 +1,9 @@
 __all__ = ['ContextSettings', 'Event', 'Joystick', 'Keyboard', 'Mouse', 'VideoMode', 'Window']
 
-from sfml.system import *
 from sfml.graphics.graphics import ContextSettings, Event, Joystick, Keyboard, Mouse, VideoMode, Window
 
+from sfml.graphics.graphics import SFMLException
+from sfml.graphics.graphics import sleep, Clock
+from sfml.system.position import Position
+from sfml.system.size import Size
+from sfml.system.rectangle import Rectangle
