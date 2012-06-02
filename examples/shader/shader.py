5ada57dd906ba6dc2b310f0f20815545c5428204
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2012-06-02 22:38:59 +0200

    Forgot to add the license to shader.py and accidentally uploaded Shader.cpp and Effect.hpp.

--- examples/shader/shader.py
+++ examples/shader/shader.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# pySFML2 - Cython SFML Wrapper for Python
+# Copyright 2012, Jonathan De Wachter <dewachter.jonathan@gmail.com>
+#
+# This software is released under the GPLv3 license.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from random import randint
 from math import cos
 
