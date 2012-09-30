ee393b1ba66fa423b66542538724f0d20d47d521
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2012-09-30 02:53:30 +0200

    Ported voip example (#21)

---
+++ examples/voip/client.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# pySFML2 - Cython SFML Wrapper for Python
+# Copyright 2012, Jonathan De Wachter <dewachter.jonathan@gmail.com>
+#
+# This software is released under the GPLv3 license.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sfml as sf
+from struct import pack
+from random import randint
+AUDIO_DATA, END_OF_STREAM = list(range(1, 3))
+
+class NetworkRecorder(sf.SoundRecorder):
+	def __init__(self, host, port):
+		sf.SoundRecorder.__init__(self)
+		
+		self.host = host # address of the remote host
+		self.port = port # remote port
+		self.socket = sf.TcpSocket() # socket used to communicate with the server
+		
+	def on_start(self):
+		try: self.socket.connect(self.host, self.port)
+		except sf.SocketException as error: return False
+
+		return True
+			
+	def on_process_samples(self, chunk):
+		# pack the audio samples 
+		data = pack("B", AUDIO_DATA)
+		data += pack("I", len(chunk.data))
+		data += chunk.data
+		
+		# send the audio packet
+		try: self.socket.send(data)
+		except sf.SocketException: return False
+		
+		return True
+		
+	def on_stop(self):
+		# send a "end-of-stream" signal
+		self.socket.send(bytes(END_OF_STREAM))
+		
+		# close the socket
+		self.socket.disconnect()
+
+def do_client(port):
+	# check that the device can capture audio
+	if not sf.SoundRecorder.is_available():
+		print("Sorry, audio capture is not supported by your system")
+		return
+
+	# ask for server address
+	server = input("Type address or name of the server to connect to: ")
+	server = sf.IpAddress.from_string(server)
+
+	# create an instance of our custom recorder
+	recorder = NetworkRecorder(server, port)
+
+	# wait for the user input...
+	input("Press enter to start recording audio")
+
+	# start capturing audio data
+	recorder.start(44100)
+	input("Recording... press enter to stop")
+	recorder.stop()
---
+++ examples/voip/voip.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# pySFML2 - Cython SFML Wrapper for Python
+# Copyright 2012, Jonathan De Wachter <dewachter.jonathan@gmail.com>
+#
+# This software is released under the GPLv3 license.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sfml as sf
+import client, server
+
+# choose a random port for opening sockets (ports < 1024 are reserved)
+PORT = 2435
+
+# client or server ?
+print("Do you want to be a server (s) or a client (c) ?")
+who = input()
+
+if who == 's':
+	server.do_server(PORT)
+else:
+	client.do_client(PORT)
+
+input("Press any key to exit...")
