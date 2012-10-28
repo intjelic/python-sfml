3a9398a471380acbb1c3975c6b0847be73123d73
Edwin Marshall (aspidites) <emarshall85@gmail.com>
2012-10-28 16:40:02 -0400

    - updated setup.py to include my name, cleaned up a couple of things, converted tabs to spaces

--- setup.py
+++ setup.py
@@ -14,103 +14,96 @@ from distutils.core import setup, Command
 from distutils.extension import Extension
 
 class Test(Command):
-	user_options = []
-	def initialize_options(self): pass
-	def finalize_options(self): pass
+    user_options = []
+    def initialize_options(self): pass
+    def finalize_options(self): pass
 
-	def run(self):
-		import sys, subprocess
-		errno = subprocess.call([sys.executable, '-m',
-								 'unittest', 'discover', '-s', 'tests'])
+    def run(self):
+        import sys, subprocess
+        errno = subprocess.call([sys.executable, '-m',
+                                 'unittest', 'discover', '-s', 'tests'])
 
 
-USE_CYTHON = os.environ.get('USE_CYTHON', False)
+if os.environ.get('USE_CYTHON'):
+    import Cython.Distutils
 
-if USE_CYTHON:
-	import Cython.Distutils
-
-	x11_source = 'src/sfml/x11.pyx'
-	system_source = 'src/sfml/system.pyx'
-	window_source = 'src/sfml/window.pyx'
-	graphics_source = 'src/sfml/graphics.pyx'
-	audio_source = 'src/sfml/audio.pyx'
-	network_source = 'src/sfml/network.pyx'
+    x11_source = 'src/sfml/x11.pyx'
+    system_source = 'src/sfml/system.pyx'
+    window_source = 'src/sfml/window.pyx'
+    graphics_source = 'src/sfml/graphics.pyx'
+    audio_source = 'src/sfml/audio.pyx'
+    network_source = 'src/sfml/network.pyx'
 
 else:
-	x11_source = 'src/sfml/x11.cpp'
-	system_source = 'src/sfml/system.cpp'
-	window_source = 'src/sfml/window.cpp'
-	graphics_source = 'src/sfml/graphics.cpp'
-	audio_source = 'src/sfml/audio.cpp'
-	network_source = 'src/sfml/network.cpp'
+    x11_source = 'src/sfml/x11.cpp'
+    system_source = 'src/sfml/system.cpp'
+    window_source = 'src/sfml/window.cpp'
+    graphics_source = 'src/sfml/graphics.cpp'
+    audio_source = 'src/sfml/audio.cpp'
+    network_source = 'src/sfml/network.cpp'
 
 x11 = Extension('sfml.x11',
-				[x11_source],
-				['include', 'src/sfml'],
-				language='c++',
-				libraries=['X11'])
+                [x11_source],
+                ['include', 'src/sfml'],
+                language='c++',
+                libraries=['X11'])
 
 system = Extension('sfml.system',
-					[system_source, 'src/sfml/error.cpp'],
-					['include', 'src/sfml'],
-					language='c++',
-					libraries=['sfml-system', 'sfml-graphics'])
+                    [system_source, 'src/sfml/error.cpp'],
+                    ['include', 'src/sfml'],
+                    language='c++',
+                    libraries=['sfml-system', 'sfml-graphics'])
 
 window = Extension('sfml.window',
-					[window_source, 'src/sfml/derivablewindow.cpp'],
-					['include', 'src/sfml'],
-					language='c++',
-					libraries=['sfml-system', 'sfml-window'])
+                    [window_source, 'src/sfml/derivablewindow.cpp'],
+                    ['include', 'src/sfml'],
+                    language='c++',
+                    libraries=['sfml-system', 'sfml-window'])
 
 graphics = Extension('sfml.graphics',
-					[graphics_source, 'src/sfml/derivablerenderwindow.cpp', 'src/sfml/derivabledrawable.cpp'],
-					['include', 'src/sfml'],
-					language='c++',
-					libraries=['sfml-system', 'sfml-window', 'sfml-graphics'])
+                    [graphics_source, 'src/sfml/derivablerenderwindow.cpp', 'src/sfml/derivabledrawable.cpp'],
+                    ['include', 'src/sfml'],
+                    language='c++',
+                    libraries=['sfml-system', 'sfml-window', 'sfml-graphics'])
 
 audio = Extension('sfml.audio',
-					[audio_source, 'src/sfml/derivablesoundrecorder.cpp', 'src/sfml/derivablesoundstream.cpp'],
-					['include', 'src/sfml'],
-					language='c++',
-					libraries=['sfml-system', 'sfml-audio'])
+                    [audio_source, 'src/sfml/derivablesoundrecorder.cpp', 'src/sfml/derivablesoundstream.cpp'],
+                    ['include', 'src/sfml'],
+                    language='c++',
+                    libraries=['sfml-system', 'sfml-audio'])
 
 network = Extension('sfml.network',
-					[network_source],
-					['include', 'src/sfml'],
-					language='c++',
-					libraries=['sfml-system', 'sfml-network'])
+                    [network_source],
+                    ['include', 'src/sfml'],
+                    language='c++',
+                    libraries=['sfml-system', 'sfml-network'])
 
 with open('README', 'r') as f:
-	long_description = f.read()
+    long_description = f.read()
 
-major, minor, micro, releaselevel, serial = sys.version_info
+kwargs = dict(
+            name='pySFML2',
+            ext_modules=[x11, system, window, graphics, audio, network],
+            package_dir={'': 'src'},
+            packages=['sfml'],
+            version='1.2.0',
+            description='The non-official Python binding for SFML2',
+            long_description=long_description,
+            author='Jonathan de Wachter, Edwin O Marshall'.decode('UTF-8'),
+            author_email='dewachter.jonathan@gmail.com, emarshall85@gmail.com',
+            url='http://openhelbreath.be/python-sfml2',
+            license='LGPLv3',
+            classifiers=['Development Status :: 5 - Production/Stable',
+                        'Intended Audience :: Developers',
+                        'License :: OSI Approved :: GNU General Public License v3 (LGPLv3)',
+                        'Operating System :: OS Independent',
+                        'Programming Language :: Cython',
+                        'Topic :: Games/Entertainment',
+                        'Topic :: Multimedia',
+                        'Topic :: Software Development :: Libraries :: Python Modules'],
+            cmdclass = {'test': Test})
 
-kwargs = dict(name='pySFML2',
-			ext_modules=[x11, system, window, graphics, audio, network],
-			package_dir={'': 'src'},
-			packages=['sfml'],
-			version='1.2.0',
-			description='The non-official Python binding for SFML2',
-			long_description=long_description,
-			author_email='dewachter.jonathan@gmail.com',
-			url='http://openhelbreath.be/python-sfml2',
-			license='LGPLv3',
-			classifiers=['Development Status :: 5 - Production/Stable',
-						'Intended Audience :: Developers',
-						'License :: OSI Approved :: GNU General Public License v3 (LGPLv3)',
-						'Operating System :: OS Independent',
-						'Programming Language :: Cython',
-						'Topic :: Games/Entertainment',
-						'Topic :: Multimedia',
-						'Topic :: Software Development :: Libraries :: Python Modules'],
-			cmdclass = {'test': Test})
-
-if major == 2:
-	kwargs.update(author='Jonathan De Wachter'.decode())
-else:
-	kwargs.update(author='Jonathan De Wachter')
-
-if USE_CYTHON:
-	kwargs.update(cmdclass={'build_ext': Cython.Distutils.build_ext})
+if os.environ.get('USE_CYTHON')
+    kwargs.update(cmdclass={'build_ext': Cython.Distutils.build_ext})
 
 setup(**kwargs)
