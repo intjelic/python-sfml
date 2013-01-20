a4f5018370ab5d4c24becb56e434ca6e9b7400fa
Edwin Marshall (aspidites) <emarshall85@gmail.com>
2013-01-20 11:58:59 -0500

    Back-ported the setup script from latest_sfml branch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # pySFML - Python bindings for SFML
-# Copyright 2013, Jonathan De Wachter <dewachter.jonathan@gmail.com>,
+# Copyright 2012-2013, Jonathan De Wachter <dewachter.jonathan@gmail.com>,
 #				  Edwin Marshall <emarshall85@gmail.com>
 #
 # This software is released under the LGPLv3 license.
@@ -12,41 +12,61 @@
 import sys
 import os
 import platform
+from glob import glob
+from subprocess import call
+from distutils.core import setup, Command, Extension
 
-try:
-	from setuptools import setup
-	from setuptools.command.test import test
-	from setuptools.extension import Extension
-	USE_TEST=True
-except:
-	from distutils.core import setup, Extension
-	USE_TEST=False
-	
 # python 2.* compatability
 try: input = raw_input
 except NameError: pass
 
-if USE_TEST:
-	class PyTest(test):
-		def finalize_options(self):
-			test.finalize_options(self)
-			self.test_args = []
-			self.test_suite = True
+class PyTest(Command):
+	user_options = []
 
-		def run_tests(self):
-			import pytest
-			errno = pytest.main(self.test_args)
-			sys.exit(errno)
+	def initialize_options(self): pass
+	def finalize_options(self): pass
+	def run(self):
+		errno = call([sys.executable, 'runtests.py'])
+		raise SystemExit(errno)
 
+try:
+	from Cython.Distutils import build_ext as Cython
+except ImportError:
+	class Cython(Command):
+		""" Minimal command that runs 'cython' from the command line,
+			rather than importing the version-specific distutils module
+			included with Cython.
+		"""
+		user_options = []
+
+		def initialize_options(self): pass
+		def finalize_options(self): pass
+		def run(self):
+			srcdir = 'src/sfml/'
+			modules = glob(srcdir + '*.pyx')
+			errno = 1
+			if platform.system() == 'Windows': modules.remove('x11.pyx')
+
+			for module in modules:
+				try:
+					errno = call('cython --cplus {0}{1} -Iinclude'.format(srcdir, module),
+								shell=True)
+				except OSError:
+					print("Please install cython and try again.")
+				finally:
+					raise SystemExit(errno)
+
+
+sources = dict(
+	x11 = 'src/sfml/x11.cpp',
+	system = 'src/sfml/system.cpp',
+	window = 'src/sfml/window.cpp',
+	graphics = 'src/sfml/graphics.cpp',
+	audio = 'src/sfml/audio.cpp',
+	network = 'src/sfml/network.cpp')
 
 # check if cython is needed (if c++ files are generated or not)
-NEED_CYTHON = not all(map(os.path.exists, [
-	'src/sfml/x11.cpp',
-	'src/sfml/system.cpp',
-	'src/sfml/window.cpp',
-	'src/sfml/graphics.cpp',
-	'src/sfml/audio.cpp',
-	'src/sfml/network.cpp']))
+NEED_CYTHON = not all(map(os.path.exists, sources.values()))
 
 try:
 	USE_CYTHON = NEED_CYTHON or bool(int(os.environ.get('USE_CYTHON', 0)))
@@ -54,38 +74,7 @@ except ValueError:
 	USE_CYTHON = NEED_CYTHON or bool(os.environ.get('USE_CYTHON'))
 
 if USE_CYTHON:
-	try:
-		from Cython.Distutils import build_ext
-	except ImportError:
-		from subprocess import call
-		try:
-			if platform.system() != 'Windows':
-				call(["cython", "--cplus", "src/sfml/x11.pyx", "-Iinclude"])
-			call(["cython", "--cplus", "src/sfml/system.pyx", "-Iinclude"])
-			call(["cython", "--cplus", "src/sfml/window.pyx", "-Iinclude"])
-			call(["cython", "--cplus", "src/sfml/graphics.pyx", "-Iinclude"])
-			call(["cython", "--cplus", "src/sfml/audio.pyx", "-Iinclude"])
-			call(["cython", "--cplus", "src/sfml/network.pyx", "-Iinclude"])
-			USE_CYTHON = False
-		except OSError:
-			print("Please install the correct version of cython and run again.")
-			sys.exit(1)
-
-if USE_CYTHON:
-	x11_source = 'src/sfml/x11.pyx'
-	system_source = 'src/sfml/system.pyx'
-	window_source = 'src/sfml/window.pyx'
-	graphics_source = 'src/sfml/graphics.pyx'
-	audio_source = 'src/sfml/audio.pyx'
-	network_source = 'src/sfml/network.pyx'
-
-else:
-	x11_source = 'src/sfml/x11.cpp'
-	system_source = 'src/sfml/system.cpp'
-	window_source = 'src/sfml/window.cpp'
-	graphics_source = 'src/sfml/graphics.cpp'
-	audio_source = 'src/sfml/audio.cpp'
-	network_source = 'src/sfml/network.cpp'
+	sources = {k: v.replace('cpp', 'pyx') for k, v in sources.items()}
 
 extension = lambda name, files, libs: Extension(
 	'sfml.' + name,
@@ -93,30 +82,30 @@ extension = lambda name, files, libs: Extension(
 	['include', 'src/sfml'], language='c++',
 	libraries=libs)
 
-x11 = extension('x11', [x11_source], ['X11'])
+x11 = extension('x11', [sources['x11']], ['X11'])
 
 system = extension(
 	'system',
-	[system_source, 'src/sfml/error.cpp'],
+	[sources['system'], 'src/sfml/error.cpp'],
 	['sfml-system', 'sfml-graphics'])
 
 window = extension(
-	'window', [window_source, 'src/sfml/derivablewindow.cpp'],
+	'window', [sources['window'], 'src/sfml/derivablewindow.cpp'],
 	['sfml-system', 'sfml-window'])
 
 graphics = extension(
 	'graphics',
-	[graphics_source, 'src/sfml/derivablerenderwindow.cpp', 'src/sfml/derivabledrawable.cpp'],
+	[sources['graphics'], 'src/sfml/derivablerenderwindow.cpp', 'src/sfml/derivabledrawable.cpp'],
 	['sfml-system', 'sfml-window', 'sfml-graphics'])
 
 audio = extension(
 	'audio',
-	[audio_source, 'src/sfml/derivablesoundrecorder.cpp', 'src/sfml/derivablesoundstream.cpp'],
+	[sources['audio'], 'src/sfml/derivablesoundrecorder.cpp', 'src/sfml/derivablesoundstream.cpp'],
 	['sfml-system', 'sfml-audio'])
 
 network = extension(
 	'network',
-	[network_source],
+	[sources['network']],
 	['sfml-system', 'sfml-network'])
 
 
@@ -125,48 +114,34 @@ network = extension(
 # on Unix:    /usr/include/pythonX.Y/sfml/*.pxd and *.h
 
 # define the include directory
+headers = glob('src/sfml/*.h')
 if platform.system() == 'Windows':
-	include_dir = sys.prefix + "\\include\\pysfml\\"
+	include_dir = os.path.join(sys.prefix, 'include', 'pysfml')
+	headers.pop('x11.h')
 else:
 	major, minor, _, _ , _ = sys.version_info
-	include_dir = sys.prefix + "/include/python{0}.{1}/sfml/".format(major, minor)
+	include_dir = os.path.join(
+		sys.prefix, 'include', 'python{0}.{1}'.format(major, minor), 'pysfml')
 
 # list all relevant headers (find in include/ and src/sfml/)
 # key: directory, value: list of headers to place in the directory
-destinations = dict()
+files = {root: [os.path.join(root, fname) for fname in fnames]
+		 for root, dirs, fnames in os.walk('include')}
+files['include'] += glob('src/sfml/*.h')
+files = [(k.replace('include', include_dir), v) for k, v in files.items()]
 
-for path, subdirs, files in os.walk("include"):
-	# don't forget to remove include/ from the path
-	destination = include_dir + path[8:]
-	destinations[destination] = []
-
-	for name in files:
-		destinations[destination].append(os.path.join(path, name))
-
-for path, subdirs, files in os.walk("src/sfml"):
-	for name in files:
-		if name.endswith(".h"):
-			destinations[include_dir].append(os.path.join(path, name))
-	
-# format data (list of tuple)
-data_files = []
-for key in destinations:
-	data_files.append((key, destinations[key]))
-	
 with open('README.rst', 'r') as f:
 	long_description = f.read()
 
-if platform.system() == 'Windows':
-	ext_modules=[system, window, graphics, audio, network]
-else:
-	ext_modules=[x11, system, window, graphics, audio, network]
-	
+ext_modules=[x11, system, window, graphics, audio, network]
+if platform.system() == 'Windows': ext_modules.pop(x11)
+
 kwargs = dict(
 			name='pySFML',
 			ext_modules=ext_modules,
 			package_dir={'': 'src'},
 			packages=['sfml'],
-			data_files=data_files,
+			data_files=files,
 			version='1.2.0',
 			description='Python bindings for SFML',
 			long_description=long_description,
@@ -184,13 +159,6 @@ kwargs = dict(
 						'Topic :: Games/Entertainment',
 						'Topic :: Multimedia',
 						'Topic :: Software Development :: Libraries :: Python Modules'],
-			cmdclass=dict())
-
-if USE_TEST:
-	kwargs['tests_require']=['pytest>=2.3']
-	kwargs['cmdclass'].update({'test': PyTest})
-			
-if USE_CYTHON:
-	kwargs['cmdclass'].update({'build_ext': build_ext})
+			cmdclass={'test': PyTest, 'build_ext': Cython})
 
 setup(**kwargs)
