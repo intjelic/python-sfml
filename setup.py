ff9d3168013318400bda44eb37f3edafdbfbf74d
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2014-06-16 19:09:58 +0200

    Removed copyright header from setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
-# pySFML - Python bindings for SFML
-# Copyright 2012-2014, Jonathan De Wachter <dewachter.jonathan@gmail.com>,
-#                      Edwin Marshall <emarshall85@gmail.com>
-#
-# This software is released under the LGPLv3 license.
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-
 import sys, os, platform
 import os.path, shutil
 from glob import glob
