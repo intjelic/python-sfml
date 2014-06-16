bb6c77dc168734ee83565d6a4d2fdacf11080690
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2014-06-16 18:30:05 +0200

    Removed copyright header from examples source files
    Because it makes no sense.

--- examples/embedding/main.cpp
+++ examples/embedding/main.cpp
@@ -1,14 +1,3 @@
-////////////////////////////////////////////////////////////////////////////////
-//
-// pySFML - Python bindings for SFML
-// Copyright 2012-2013, Jonathan De Wachter <dewachter.jonathan@gmail.com>
-//
-// This software is released under the LGPLv3 license.
-// You should have received a copy of the GNU Lesser General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
-//
-////////////////////////////////////////////////////////////////////////////////
-
 // Including Python.h first is mandatory!
 #include <Python.h>
 
--- examples/extending/extension.pyx
+++ examples/extending/extension.pyx
@@ -1,13 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
-# pySFML - Python bindings for SFML
-# Copyright 2012-2013, Jonathan De Wachter <dewachter.jonathan@gmail.com>
-#
-# This software is released under the LGPLv3 license.
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-
 cimport libcpp.sfml as sf
 from pysfml.graphics cimport Image
 
