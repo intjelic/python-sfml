ddb1f8a96c326d4b7ecb53568a8e2ca98b14d4a3
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2017-02-13 00:44:27 +0700

    Updated copyright years (up to 2017)

--- src/sfml/graphics/DerivableDrawable.hpp
+++ src/sfml/graphics/DerivableDrawable.hpp
@@ -1,6 +1,6 @@
 /*
 * PySFML - Python bindings for SFML
-* Copyright (c) 2012-2016, Jonathan De Wachter <dewachter.jonathan@gmail.com>
+* Copyright (c) 2012-2017, Jonathan De Wachter <dewachter.jonathan@gmail.com>
 *
 * This file is part of PySFML project and is available under the zlib
 * license.
